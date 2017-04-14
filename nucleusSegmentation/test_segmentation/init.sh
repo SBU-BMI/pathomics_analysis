3c69bfe587f29cad9944e4606c72651ba184bcce
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-04-14 11:59:02 -0400

    added error trapping

--- nucleusSegmentation/test_segmentation/init.sh
+++ nucleusSegmentation/test_segmentation/init.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Program name
+PROGNAME=$(basename "$0")
+
+# Error trapping
+error_exit() {
+   echo "${PROGNAME}: ${1:-"Error"}" 1>&2
+   exit 1
+}
+
 image_name="sbubmi/test_segmentation"
 container_name="$USER-test_segmentation"
 
 # Build image from dockerfile
-docker build -t "$image_name" .
+docker build -t "$image_name" . || error_exit "Could not build container."
 
 # Start container
 python ../script/run_docker_segment.py start $container_name $image_name
-
