baccc88f59c9e08133b36369f81bf07b9834d0bb
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-04-17 15:00:36 -0400

    cwd

--- nucleusSegmentation/test_segmentation/test.sh
+++ nucleusSegmentation/test_segmentation/test.sh
@@ -10,10 +10,11 @@ input_file="TCGA-CS-4938-01Z-00-DX1_12560_47520_500_500_LGG.png"
 test1()
 {
   exec_id="20170412133151"
-
-  output_file="$HOME/test_out.zip"
+  cwd=$(pwd)
+  output_file="$cwd/test_out.zip"
 
   # Using Python script to run mainSegmentFeatures
+  # and copy output from container to local file system.
   python ../script/run_docker_segment.py \
   segment \
   $container_name \
@@ -36,11 +37,11 @@ test2()
   output_dir="/tmp/output"
   containerId=$(docker inspect --format '{{ .Id }}' $container_name)
 
-  docker exec $container_name mkdir -p $input_dir
-  docker exec $container_name mkdir -p $output_dir
+  docker exec -d $container_name mkdir -p $input_dir
+  docker exec -d $container_name mkdir -p $output_dir
   docker cp $input_file $container_name:$input_dir
   # Using Docker to run mainSegmentSmallImage
-  docker exec $container_name mainSegmentSmallImage $input_dir/$input_file "$output_dir/output"
+  docker exec -d $container_name mainSegmentSmallImage $input_dir/$input_file "$output_dir/output"
   docker cp $container_name:$output_dir/output_label.png .
   docker cp $container_name:$output_dir/processTestDeclump_label.png .
 }
