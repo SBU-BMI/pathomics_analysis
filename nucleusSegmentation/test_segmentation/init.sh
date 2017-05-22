ce638fbe342d5cf58ae8ac79fcec0fde04cca871
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-05-22 16:46:11 -0400

    Update init.sh

--- nucleusSegmentation/test_segmentation/init.sh
+++ nucleusSegmentation/test_segmentation/init.sh
@@ -5,15 +5,28 @@ PROGNAME=$(basename "$0")
 
 # Error trapping
 error_exit() {
-   echo "${PROGNAME}: ${1:-"Error"}" 1>&2
-   exit 1
+  echo "${PROGNAME}: ${1:-"Error"}" 1>&2
+  exit 1
 }
 
 image_name="sbubmi/test_segmentation"
 container_name="$USER-test_segmentation"
 
 # Build image from dockerfile
+echo ""
+echo "BUILDING DOCKER IMAGE"
+echo ""
+cd ../..
 docker build -t "$image_name" . || error_exit "Could not build container."
 
+# Or:
+# docker pull $image_name
+
+echo "Quick nap... 3 2 1"
+sleep 3
+
 # Start container
-python ../script/run_docker_segment.py start $container_name $image_name
+echo ""
+echo "STARTING CONTAINER"
+echo ""
+python nucleusSegmentation/script/run_docker_segment.py start $container_name $image_name
