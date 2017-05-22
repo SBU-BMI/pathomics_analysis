5a51827aa8429e7ca19990ae3806b70f38b09e6f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-05-22 10:24:56 -0400

    BUG: Fix include sys/time.h  (Windows build)

--- nucleusSegmentation/src/include/Normalization.h
+++ nucleusSegmentation/src/include/Normalization.h
@@ -1,4 +1,4 @@
-/*
+/**
  * Normalization.h
  *
  *  Created on: Jun 11, 2014
@@ -10,33 +10,39 @@
 
 // Includes to use opencv2/GPU
 #include "opencv2/opencv.hpp"
-//#include "opencv2/gpu/gpu.hpp"
-#include <sys/time.h>
 
+/*
+// HIDE GPU
+#ifdef WITH_CUDA
+#include "opencv2/gpu/gpu.hpp"
+#endif
+*/
+
+//#include <sys/time.h>  // not used
+
+//#include "../segment/PixelOperations.h"
+// It's in same directory:
 #include "PixelOperations.h"
 
-
-
-namespace nscale{
+namespace nscale {
 
 class Normalization {
 private:
-	static void PixelClass(cv::Mat I, cv::Mat o_fg, cv::Mat o_bg, cv::Mat& o_fg_lab, cv::Mat& o_bg_lab);
-	static cv::Mat TransferI(cv::Mat fg_lab, cv::Mat fg_mask, float meanT[3], float stdT[3]);
-	static cv::Mat bgr2Lab(cv::Mat I);
-	static cv::Mat lab2BGR(cv::Mat LAB);
-	static int rndint(float n);
+    // static cv::Mat segFG(cv::Mat I, cv::Mat M); // 'public' see below
+    static void PixelClass(cv::Mat I, cv::Mat o_fg, cv::Mat o_bg, cv::Mat& o_fg_lab, cv::Mat& o_bg_lab);
+    static cv::Mat TransferI(cv::Mat fg_lab, cv::Mat fg_mask, float meanT[3], float stdT[3]);
+    static cv::Mat bgr2Lab(cv::Mat I);
+    static cv::Mat lab2BGR(cv::Mat LAB);
+    static int rndint(float n);
 
 public:
-	// normalization operations that mimics our matlab code. It uses as an input the BGR image and
-	// mean/std of the lab channels computed from the target image using the function targetParameters bellow.
-	static cv::Mat normalization(const cv::Mat& originalI, float targetMean[3], float targetStd[3]);
-	static void targetParameters(const cv::Mat& originalI, float (&targetMean)[3], float (&targetStd)[3]);
-
-	static cv::Mat segFG(cv::Mat I, cv::Mat M);
-
+    // normalization operations that mimics our matlab code. It uses as an input the BGR image and
+    // mean/std of the lab channels computed from the target image using the function targetParameters bellow.
+    static cv::Mat normalization(const cv::Mat& originalI, float targetMean[3], float targetStd[3]);
+    static void targetParameters(const cv::Mat& originalI, float(&targetMean)[3], float(&targetStd)[3]);
+    // segFG() must be public; being called from utilityTileAnalysis.h
+    static cv::Mat segFG(cv::Mat I, cv::Mat M);
 };
 
-
-}// end nscale
+} // end nscale
 #endif /* NORMALIZATION_H_ */
